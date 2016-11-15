e186a23e460d5e3554e9d9deefd634178ffde5b8
Michael Schmidt <mschmidty@gmail.com>
2016-11-14 18:09:32 -0700

    finsished home and about

--- about.php
+++ about.php
@@ -4,38 +4,39 @@ Template Name: About
 */
 ?>
 <?php get_header(); ?>
+
 <div class="content about">
-<div class="grid">
-	<h2>About Us</h2>
-	<?php
+	<div class="grid">
+		<h2>About Us</h2>
+		<?php
 
-    $custom_query = new WP_Query(array(
-        'posts_per_page' => 10,
-        'cat' => '3',
-        'paged' => (get_query_var('paged')) ? get_query_var('paged') : 1
-    ));
+			$custom_query = new WP_Query(array(
+			'posts_per_page' => 10,
+			'cat' => '3',
+			'paged' => (get_query_var('paged')) ? get_query_var('paged') : 1
+			));
 
-    if ( $custom_query->have_posts() ) :
-	                while ( $custom_query->have_posts() ) : $custom_query->the_post();
-	                	echo '<div class="col col-1-1">';
+			if ( $custom_query->have_posts() ) :
+			while ( $custom_query->have_posts() ) : $custom_query->the_post();
+			echo '<div class="col col-1-1">';
 
-	                    if (has_post_thumbnail()) {
-	                    	echo '<div class="about-thumb"><div class="circle">';
-	                    	echo the_post_thumbnail();
-	                    	echo '</div></div>';
-	                    }
+			if (has_post_thumbnail()) {
+			echo '<div class="about-thumb"><div class="circle">';
+			echo the_post_thumbnail();
+			echo '</div></div>';
+			}
 
-	                    echo '<h3>' . get_the_title() . '</h3>';
-	                    echo '<p>' . the_content() .'</p>';
+			echo '<h3>' . get_the_title() . '</h3>';
+			echo '<p>' . the_content() .'</p>';
 
-	                   	echo '</div>';
+			echo '</div>';
 
-	                endwhile;
-	                wp_reset_query();
-                    endif;
+			endwhile;
+			wp_reset_query();
+			endif;
 
-	?>
-</div>
-</div>
+		?>
+	</div> <!--.grid-->
+</div><!--.content.about-->
 
 <?php get_footer(); ?>
