0fec387386267e311eb43d96fff85856eba57b73
Michael Schmidt <mschmidty@gmail.com>
2016-12-16 16:14:20 -0700

    finidhed products landing

--- products.php
+++ products.php
@@ -9,7 +9,7 @@ Template Name: Products
 
   <div class="grid">
     <h2>Products</h2>
-    <div class="products-wrap">
+    <div class="products-wrap products flex-parent">
 
       <?php
 
@@ -28,17 +28,35 @@ Template Name: Products
 
           <?php while ( $parent->have_posts() ) : $parent->the_post(); ?>
 
-              <div id="parent-<?php the_ID(); ?>" class="parent-page col col-1-3">
+                <div id="parent-<?php the_ID(); ?>" class="parent-page flex-child products-thumb">
+                    <a href="<?php the_permalink(); ?>" class= "products-link">
+                    <?php
+                      $image = get_field('landing_image');
+                      $size = 'thumb-img';
+                      $landing_image = $image['sizes'][$size];
+                    ?>
+                    <?php
+                    if( !empty($image) ): ?>
+                      <img src="<?php echo $landing_image; php?>" alt="<?php echo $landing_image['alt']; php?>" />
+                    <?php
+                    endif; ?>
 
-                  <h1><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h1>
+                    <h4 class="products-thumb-link"><?php the_title(); ?></h4>
+                  <?php if(get_field('landing_blurb')): ?>
+                    <p><?php the_field('landing_blurb'); ?></p>
+                  <?php endif; ?>
+                  </a>
+                </div>
 
-                  <p><?php the_excerpt(); ?></p>
 
-              </div>
+
 
           <?php endwhile; ?>
 
       <?php endif; ?>
+        <div class="flex-child"></div>
+        <div class="flex-child"></div>
+        <div class="flex-child"></div>
     </div> <!--thumb-area-->
 
   </div> <!--grid-->
